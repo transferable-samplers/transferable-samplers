a727ebea90c14867381e6ecf843ebc2f59ecf62e
Charlie Tan <76977995+charliebtan@users.noreply.github.com>
2025-12-15 13:33:08 +0000

    fixing 8AA tica models

--- src/evaluation/metrics/jsd_metric.py
+++ src/evaluation/metrics/jsd_metric.py
@@ -6,7 +6,7 @@ import torch
 from scipy.spatial.distance import jensenshannon
 from sklearn.cluster import KMeans
 
-from src.data.preprocessing.tica import tica_features, tica_features_ca
+from src.data.preprocessing.tica import tica_features
 
 
 def compute_hist(states, n_clusters=20):
@@ -29,13 +29,8 @@ def jsd_metric(true_samples, pred_samples, topology, tica_model=None, tica_model
     true_traj_samples = md.Trajectory(true_samples.cpu().numpy(), topology=topology)
     pred_traj_samples = md.Trajectory(pred_samples.cpu().numpy(), topology=topology)
 
-    if topology.n_residues > 4:
-        ca_list = [atom.index for atom in topology.atoms if atom.name == "CA"]
-        features_test = tica_features_ca(true_traj_samples, ca_list=ca_list)
-        features = tica_features_ca(pred_traj_samples, ca_list=ca_list)
-    else:
-        features_test = tica_features(true_traj_samples)
-        features = tica_features(pred_traj_samples)
+    features_test = tica_features(true_traj_samples)
+    features = tica_features(pred_traj_samples)
 
     n = min(len(features_test), len(features))
     tics_test = torch.Tensor(tica_model.transform(features_test))[:n, 0:2]
