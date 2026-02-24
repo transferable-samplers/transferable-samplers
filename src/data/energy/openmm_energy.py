f26c4f30cf485a27e82a6ca26a2f169ea97ce5eb
charliebtan <76977995+char-tan@users.noreply.github.com>
2026-02-24 15:17:53 +0900

    remove invalid platform settings for openmm energy

--- src/data/energy/openmm_energy.py
+++ src/data/energy/openmm_energy.py
@@ -63,9 +63,7 @@ class OpenMMEnergy:
         if platform_name == "CPU":
             local_world_size = int(os.environ.get("LOCAL_WORLD_SIZE", 1))
             properties["Threads"] = str(os.cpu_count() // local_world_size)
-            properties["Precision"] = "mixed"
         elif platform_name == "CUDA":
-            properties["Precision"] = "mixed"
             if device_index is not None:
                 properties["DeviceIndex"] = str(device_index)
 
